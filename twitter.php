cd6bb0674c2d0ca29c647c00273a32bf370aa7bf
Neya Rangesh <hello@marketconvert.com>
2015-04-23 13:16:50 +0800

    Initial commit

--- twitter.php
+++ twitter.php
@@ -4,30 +4,37 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-define('CONSUMER_KEY', 'QbpyNZyNDrzkmgUaQr1D7KNse');
-define('CONSUMER_SECRET', 'CWjRaL4CxK7lCpydG8Fjxf7tVmvSXWJfuqrMnvz6gA2bqa5FPz');
-define('ACCESS_TOKEN', '402077723-cQ4FJ7JaYdIrkVmZRsJTp0VUdeKRkoziheKQqeXU');
-define('ACCESS_TOKEN_SECRET', 'gOBL8hPLKN5uVepog6tCOF2JdC5jknKHx3cuNAL4jmU5J');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('ACCESS_TOKEN', '');
+define('ACCESS_TOKEN_SECRET', '');
 
 $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 $max_id = "";
-foreach (range(1, 10) as $i) { // up to 10 result pages
+foreach (range(1, 20) as $i) { // up to 20 result pages
 
   $query = array(
-    "q" => "#ff7 since:2014-03-27 until:2015-04-15", // Change here
-    "count" => 20,
+    "q" => "#transformers4 since:2014-06-20 until:2014-07-09", // Change here
+    "count" => 100,
     "result_type" => "mixed",
     "max_id" => $max_id,
+    "lang" => "en"
   );
 
   $results = $toa->get('search/tweets', $query);
+  //echo json_encode($results->statuses);
 
+  print_r(json_encode($results));
+
+/*
   foreach ($results->statuses as $result) {
     echo " [" . $result->created_at . "] " . $result->user->screen_name . ": " . $result->text . "\n";
     //echo $result
     $max_id = $result->id_str; // Set max_id for the next search result page
   }
+*/
+
 }
 
 
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'parseCSV' => $vendorDir . '/parsecsv/php-parsecsv/parsecsv.lib.php',
 );
